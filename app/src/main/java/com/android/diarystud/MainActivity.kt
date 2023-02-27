3f321c55d565bd17b2728d7fd49e4f925837b63c
Evgeny Pavlusenko <evgenyp2004@gmail.com>
2023-02-27 17:20:37 +0500

    Изменено то, что должно выводиться на экран (цвет выбран рандомно). Основные изменения коснулись мейна. 18 минута

--- app/src/main/java/com/android/diarystud/MainActivity.kt
+++ app/src/main/java/com/android/diarystud/MainActivity.kt
@@ -1,34 +1,50 @@
 package com.android.diarystud
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.android.diarystud.navigation.DiaryNavHost
 import com.android.diarystud.ui.theme.DiaryStudTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DiaryStudTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    DiaryNavHost()
-                }
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = "Дневник студента")
+                            },
+                            backgroundColor = Color.Magenta,
+                            contentColor = Color.White,
+                            elevation = 12.dp
+                        )
+                    },
+                    content = {
+                        Surface(
+                            modifier = Modifier.fillMaxSize(),
+                            color = MaterialTheme.colors.background
+                        ) {
+                            DiaryNavHost()
+                        }
+                    }
+                )
             }
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
